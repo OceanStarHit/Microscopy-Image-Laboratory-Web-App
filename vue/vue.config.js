b0826bb3a95048e1e54c050e318b562f79a600af
Best Solution <bestsolution317@outlook.com>
2021-04-20 13:30:27 +0800

    Fix the project structure

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -1,10 +1,24 @@
 var path = require("path");
 
+// module.exports = {
+//     configureWebpack: {
+//         devtool: "source-map"
+//     },
+//     publicPath: "./static",
+//     indexPath: path.join("../templates", "index.html"),
+//     outputDir: path.join("../Django", "static")
+// };
+
+// const path = require("path");
+
 module.exports = {
-    configureWebpack: {
-        devtool: "source-map"
-    },
-    publicPath: "./static",
-    indexPath: path.join("../templates", "index.html"),
-    outputDir: path.join("../Django", "static")
+  transpileDependencies: ["vuetify"],
+
+  publicPath: process.env.VUE_APP_STATIC_URL,
+  outputDir: path.resolve(__dirname, "../django/static"),
+  indexPath: path.resolve(__dirname, "../django/templates", "index.html"),
+
+  configureWebpack: {
+    devtool: "source-map"
+  }
 };
