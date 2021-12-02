cfacb0378afcbaa898b67268c66fe41a34ce7d38
anon <lofgrendaniel@hotmail.com>
2021-12-02 22:57:46 +0900

    Major refactoring in regards to DB, due to issues with asyncio loop

--- mainApi/tests/auth.py
+++ mainApi/tests/auth.py
@@ -1,32 +1,12 @@
-import os
-
-import asyncio
 import time
 
 import pyotp
 import pytest
-from fastapi import Depends
 from fastapi.security import OAuth2PasswordRequestForm
-from motor.motor_asyncio import AsyncIOMotorClient
 
 from mainApi.app.auth.auth import create_user, login
 from mainApi.app.auth.models.user import ShowUserModel, LoginUserReplyModel, CreateUserModel
 
-
-#
-# @pytest.yield_fixture
-# def event_loop():
-#     """Create an instance of the default event loop for each test case."""
-#     policy = asyncio.get_event_loop_policy()
-#     res = policy.new_event_loop()
-#     asyncio.set_event_loop(res)
-#     res._close = res.close
-#     res.close = lambda: None
-#
-#     yield res
-#
-#     res._close()
-#
 from mainApi.app.db.mongodb import get_database
 from mainApi.app.db.mongodb_utils import connect_to_mongo, close_mongo_connection
 from mainApi.config import MONGO_DB_NAME
@@ -50,70 +30,15 @@ async def db():
     yield db
 
     db.drop_database(MONGO_DB_NAME)  # delete testDB after test
-    time.sleep(2)
+    time.sleep(2)  # gotta sleep here for a while because so there is time to delete the db before running next test
 
     await close_mongo_connection()
-
-
-
-# @pytest.fixture
-# def db():
-#     from motor.motor_asyncio import AsyncIOMotorClient
-#     from mainApi.config import get_settings, get_db
-#
-#     print('before db')
-#     assert os.environ.get('IS_TESTING', None) == 'true', "Must set 'IS_TESTING' env variable to 'true' before running test"
-#     os.environ.update({'IS_TESTING': 'true'})  # temporarily sets IS_TESTING environment variable
-#     settings = get_settings()
-#     assert settings.mongo_db_name == 'testDB'
-#
-#     db = get_db()
-#     assert db.name == 'testDB'
-#
-#     yield db
-#
-#     # client = AsyncIOMotorClient(settings.mongo_url, io_loop=event_loop)
-#     client = AsyncIOMotorClient(settings.mongo_url)
-#
-#     client.drop_database('testDB')  # delete testDB after test
-#     print('after db')
-
-
-# @pytest.fixture
-# def db():
-#     # from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
-#     from mainApi.config import get_settings, get_db
-#     settings = get_settings()
-#
-#     print('before db')
-#     assert os.environ.get('IS_TESTING', None) == 'true', "Must set 'IS_TESTING' env variable to 'true' before running test"
-#     os.environ.update({'IS_TESTING': 'true'})  # temporarily sets IS_TESTING environment variable
-#     assert settings.mongo_db_name == 'testDB'
-#
-#     client = MongoClient(settings.mongo_url)
-#     db = client[settings.mongo_db_name]
-#
-#     assert db.name == 'testDB'
-#
-#     yield db
-#
-#     client.drop_database('testDB')  # delete testDB after test
-#     print('after db')
-
-# @pytest.fixture
-# def created_user():
-#     from motor.motor_asyncio import AsyncIOMotorClient, AsyncIOMotorDatabase
-#     from mainApi.config import get_settings, get_db
-#
-#
-#     yield created_user
 
 
 class TestAuth:
 
     @pytest.mark.asyncio
     async def test_create_user(self, test_user, db):
-
         users = await db[MONGO_DB_NAME]["users"].find().to_list(None)
         assert len(users) == 0
 
@@ -138,58 +63,9 @@ class TestAuth:
         user_db = await db[MONGO_DB_NAME]["users"].find_one({"_id": str(created_user.user.id)})
         assert user_db['_id'] == str(created_user.user.id)
 
-    # @pytest.mark.asyncio
-    # async def test_login2(self, event_loop):
-    #     # start by creating a user, cannot be fixture due to async TODO
-    #     from mainApi.app.auth.models.user import CreateUserModel
-    #     from mainApi.app.auth.routers import create_user
-    #     from mainApi.app.auth.routers import login2
-    #
-    #     from motor.motor_asyncio import AsyncIOMotorClient
-    #     from mainApi.config import get_settings, get_db
-    #
-    #     print('before db')
-    #     assert os.environ.get('IS_TESTING',
-    #                           None) == 'true', "Must set 'IS_TESTING' env variable to 'true' before running test"
-    #     os.environ.update({'IS_TESTING': 'true'})  # temporarily sets IS_TESTING environment variable
-    #     settings = get_settings()
-    #     assert settings.mongo_db_name == 'testDB'
-    #
-    #     db = get_db()
-    #     assert db.name == 'testDB'
-    #
-    #     client = AsyncIOMotorClient(settings.mongo_url, io_loop=event_loop)
-    #
-    #     client.drop_database('testDB')  # delete testDB after test
-    #     print('after db')
-    #
-    #
-    #     user_to_create = CreateUserModel(
-    #         full_name="Test User",
-    #         email="test@test.com",
-    #         password="password",  # plain text password
-    #     )
-    #
-    #     created_user = await create_user(user_to_create)
-    #
-    #     # start of login
-    #     form = OAuth2PasswordRequestForm(username=created_user.user.email, password='password', scope="")
-    #     otp = pyotp.TOTP(created_user.otp_secret)
-    #
-    #     login_reply: LoginUserReplyModel = await login2(form, otp=otp.now())
-    #
-    #     assert login_reply is not None
-    #     assert login_reply.user.email == user_to_create.email
-    #     assert login_reply.access_token is not None
-    #     assert login_reply.token_type == 'bearer'
-
-
-
     @pytest.mark.asyncio
-    async def test_login2(self, test_user, db):
+    async def test_login(self, test_user, db):
         # start by creating a user, cannot be fixture due to async TODO
-
-
 
         created_user = await create_user(test_user, db)
 
@@ -203,4 +79,3 @@ class TestAuth:
         assert login_reply.user.email == test_user.email
         assert login_reply.access_token is not None
         assert login_reply.token_type == 'bearer'
-
