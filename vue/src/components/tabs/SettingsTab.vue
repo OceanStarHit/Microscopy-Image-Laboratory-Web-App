1bad6595f804aa523064062a396274a0b271de02
daisukekubota <daisukekubota@outlook.jp>
2022-01-15 19:36:20 +0900

    test
    test

--- vue/src/components/tabs/SettingsTab.vue
+++ vue/src/components/tabs/SettingsTab.vue
@@ -1,13 +1,23 @@
 <template>
-  <tab-item title="Settings"></tab-item>
+  <tab-item title="Settings">
+    <small-card title="Objective">
+      <v-divider></v-divider>
+    <Objective />
+    </small-card>
+  </tab-item>
 </template>
 
 <script>
 import TabItem from "../custom/TabItem";
+import Objective from "./contents/viewcontrol/Objective";
+
 export default {
   name: "SettingsTab",
 
-  components: { TabItem },
+  components: { 
+    TabItem,
+    Objective 
+  },
 
   data: () => ({})
 };
