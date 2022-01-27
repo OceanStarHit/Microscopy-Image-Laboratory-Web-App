a4eed6dc8ca94322fe39bdbdc1c558c1c1169208
MeenoySeesongkham <49068520+nayjanta@users.noreply.github.com>
2022-01-28 00:18:51 +0700

    Update Scale.vue

--- vue/src/components/tabs/contents/adjust/Scale.vue
+++ vue/src/components/tabs/contents/adjust/Scale.vue
@@ -2,23 +2,29 @@
   <small-card title="Scale">
     <custom-button icon="ruler" label="ScaleBar" @click="select1" />
     <custom-button icon="grid" label="Grid" @click="select2" />
-    <custom-button icon="cog" label="Set" @click="select3" />
+    <custom-button icon="cog" label="Set" @click="scaleSetDialog = true" />
+    
+    <open-scale-set-dialog v-model="scaleSetDialog" />
+
   </small-card>
 </template>
 
 <script>
 import SmallCard from "../../../custom/SmallCard";
 import CustomButton from "../../../custom/CustomButton";
-
+import OpenScaleSetDialog from "./OpenScaleSetDialog";
 export default {
   name: "Scale",
 
   components: {
     SmallCard,
-    CustomButton
+    CustomButton,
+    OpenScaleSetDialog
   },
 
-  data: () => ({}),
+  data: () => ({
+    scaleSetDialog: false,    
+  }),
 
   methods: {
     select1: function() {
